f18aa00654257464761a9ba8feb1d290650b7c3e
NickyHe <nickyhe123@gmail.com>
2019-01-09 17:15:29 +1300

    responsive layout

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Nick He on 9/01/19.
+//  Copyright © 2019 Nick He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
